2cff6d8aab06bd6324112e38e0e6130df0498282
Ramiro Araujo <rama.araujo@gmail.com>
2013-12-13 21:36:48 -0300

    changed bundle install and update to use OS bundler app to install dependencies, to avoid segmentation faults, and changed to use /usr/bin/ruby

--- workflow/main.rb
+++ workflow/main.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-require_relative 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
+require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
 require_relative "bundle/bundler/setup"
 require "sqlite3"
 require "alfred"
