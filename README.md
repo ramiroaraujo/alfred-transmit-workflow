3202001f7213de7775a2686bbd965b187b67d713
Ramiro Araujo <rama.araujo@gmail.com>
2014-01-20 16:26:32 -0300

    merged support for Favorites.xml file from master branch

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 > Note, this is the pre-mavericks branch. For the Mavericks branch go to the [master branch](https://github.com/ramiroaraujo/alfred-transmit-workflow)
 
-Workflow for searching and opening Favorites in Transmit 4 App. It's really _fast_, because it reads the SQLite Database
-in latests releases of Transmit 4.
+Workflow for searching and opening Favorites in Transmit 4 App. It's really _fast_, because it reads the SQLite Database or XML datasource in latests releases of Transmit 4.
 
 There are already at least 2 Transmit workflows, but one is incompatible with latests Transmit 4 and the other, although very good, uses AppleScript to do the searching, and thus you need to wait for Transmit to open to get feedback. This is particulary slow on non SSD machines.
 
@@ -21,3 +20,4 @@ For Previous OS X Versions, Download the [alfred-transmit-workflow.alfredworkflo
 * _2013-12-16_ - Released
 * _2014-01-02_ - Added support for previous OS versions, using System Ruby 1.8, tested up to Lion
 * _2014-01-03_ - Search in both Favorite name and host
+* _2014-01-20_ - Added support for Favorites.xml
--- workflow/main.rb
+++ workflow/main.rb
@@ -2,20 +2,30 @@
 # encoding: utf-8
 
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
-require "bundle/bundler/setup"
-require "sqlite3"
-require "alfred"
+require 'bundle/bundler/setup'
+require 'nokogiri'
+require 'sqlite3'
+require 'alfred'
 
 query = ARGV[0]
+query = query.downcase if query
 
-favorites = '~/Library/Application Support/Transmit/Favorites/Favorites.sqlite'
+favorites_folder = '~/Library/Application Support/Transmit/Favorites/'
+favorites_sqlite = 'Favorites.sqlite'
+favorites_xml = 'Favorites.xml'
 
 Alfred.with_friendly_error do |alfred|
 
   fb = alfred.feedback
 
-  # check if database exists
-  if !File.exists? File.expand_path favorites
+  db_type = nil
+
+  # check if xml favorites exist first
+  if File.exists? File.expand_path favorites_folder + favorites_xml
+    db_type = 'xml'
+  elsif File.exists? File.expand_path favorites_folder + favorites_sqlite
+    db_type = 'sqlite'
+  else
     fb.add_item({
                     :uid => '',
                     :title => 'No Transmit Database found!',
@@ -28,20 +38,32 @@ Alfred.with_friendly_error do |alfred|
                 })
 
     puts fb.to_xml()
-    return
+    exit
   end
 
-  # read sqllite DB from Transmit
-  db = SQLite3::Database.open(File.expand_path favorites)
-  search = "%#{query}%"
-  db.execute("select ZUUIDSTRING, ZNICKNAME, ZUSERNAME, ZSERVER from ZOBJECT where Z2_COLLECTION = 2 AND (ZNICKNAME LIKE ? OR ZSERVER LIKE ?)", search, search) do |row|
-    fb.add_item({
-                    :uid => row[0],
-                    :title => row[1],
-                    :subtitle => "#{row[2]}@#{row[3]}",
-                    :arg => row[0],
-                    :valid => 'yes',
-                })
+  if db_type == 'xml'
+    doc = Nokogiri::XML(File.open File.expand_path favorites_folder + favorites_xml)
+    doc.xpath("//object/attribute[@name='nickname' and contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '#{query}')]/../relationship[@name='collection' and @idrefs!='z103']/..").each do |elem|
+      fb.add_item({
+                      :uid => elem.xpath("./attribute[@name='uuidstring']").text(),
+                      :title => elem.xpath("./attribute[@name='nickname']").text(),
+                      :subtitle => "#{elem.xpath("./attribute[@name='username']").text()}@#{elem.xpath("./attribute[@name='server']").text()}",
+                      :arg => elem.xpath("./attribute[@name='uuidstring']").text(),
+                      :valid => 'yes',
+                  })
+    end
+  elsif db_type == 'sqlite'
+    # read sqllite DB from Transmit
+    db = SQLite3::Database.open File.expand_path favorites_folder + favorites_sqlite
+    db.execute("select ZUUIDSTRING, ZNICKNAME, ZUSERNAME, ZSERVER from ZOBJECT where Z2_COLLECTION = 2 AND ZNICKNAME LIKE ?", "%#{query}%") do |row|
+      fb.add_item({
+                      :uid => row[0],
+                      :title => row[1],
+                      :subtitle => "#{row[2]}@#{row[3]}",
+                      :arg => row[0],
+                      :valid => 'yes',
+                  })
+    end
   end
 
   # sends "no result" message
